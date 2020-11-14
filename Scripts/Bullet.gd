4e53b58cdefd22a033f5acd4bf7f8b8d76e9176a
montinevra <23004878+montinevra@users.noreply.github.com>
2020-11-13 21:11:34 -0700

    bullets shouldn't collide with each other

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -7,8 +7,8 @@ var m_velocity = Vector2.UP * SPEED
 var window_height = ProjectSettings.get_setting("display/window/size/height")
 
 
-func _physics_process(_delta):
-	move_and_slide(m_velocity)
+func _physics_process(delta):
+	move_and_collide(m_velocity * delta)
 	if position.y < -50 or position.y > window_height + 50:
 		queue_free()
 
