b655ed108e027a674a55b66c362075811873edff
Montinevra <montinevra@gmail.com>
2020-11-13 22:17:44 -0700

    enemy moves within playfield

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,11 +1,23 @@
 extends KinematicBody2D
 
 
-const SPEED = 200
+const SPEED = 100
 const SHOOT_CD = 1
+const PLAYFIELD_EDGE = 20
 const BULLET_SCENE = preload("res://scenes/EnemyBullet.tscn")
 const BULLET_SPAWN_POS = Vector2.DOWN * 12
+var m_velocity = Vector2.RIGHT * SPEED
+var window_width = ProjectSettings.get_setting("display/window/size/width")
 onready var c_shoot_timer = $ShootTimer
+
+
+func _physics_process(delta):
+# warning-ignore:return_value_discarded
+	move_and_slide(m_velocity)
+	if position.x > window_width - PLAYFIELD_EDGE:
+		m_velocity = Vector2.LEFT * SPEED
+	elif position.x < PLAYFIELD_EDGE:
+		m_velocity = Vector2.RIGHT * SPEED
 
 
 #private:
