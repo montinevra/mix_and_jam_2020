c36adf67a244f96d0ab4b3e44ed5e1a751149491
Montinevra <23004878+montinevra@users.noreply.github.com>
2020-11-14 12:32:08 -0700

    add start game function

---
+++ Scripts/PlayField.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+const PLAYER_SCENE = preload("res://scenes/Player.tscn")
+const ENEMY_SCENE = preload("res://scenes/Enemy.tscn")
+onready var c_player_start = $PlayerStart
+onready var c_enemy_start = $EnemyStart
+
+
+func _ready():
+	_start_game()
+
+
+func _start_game():
+	var player = PLAYER_SCENE.instance()
+	var enemy = ENEMY_SCENE.instance()
+	
+	player.position = c_player_start.position
+	enemy.position = c_enemy_start.position
+	add_child(player)
+	add_child(enemy)
+
