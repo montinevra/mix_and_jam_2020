ab1233ac88ba9fbfcdbcf3053a6e62310927d1d7
Montinevra <23004878+montinevra@users.noreply.github.com>
2020-11-15 03:30:21 -0700

    add fire control

--- Scripts/Victory.gd
+++ Scripts/Victory.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("fire"):
 		get_tree().change_scene("res://Scenes/MainMenu.tscn")
